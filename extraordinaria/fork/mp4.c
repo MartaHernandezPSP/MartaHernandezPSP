e306bc2b01aef04e0798b5d948286abfa5f4d645
MartaHernandezPSP <145117481+MartaHernandezPSP@users.noreply.github.com>
2024-05-09 12:46:59 +0200

    Create mp4.c

---
+++ extraordinaria/fork/mp4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+void main()
+{
+	
+	printf("CCC \n"); 
+	
+	if (fork()!=0) 
+	{ 
+		wait(NULL);
+		printf("AAA \n"); 
+	} 
+	else 
+	{
+		printf("BBB \n"); 
+	}
+	
+	exit(0); 
+	
+
+}
